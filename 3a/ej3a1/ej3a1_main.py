f09a4873932831bdd210d51dc00597beaf0f44c5
ThisfrutaHD <268890942+ThisfrutaHD@users.noreply.github.com>
2026-04-20 16:53:30 +0200

    Enunciat

--- 3a/ej3a1/ej3a1_main.py
+++ 3a/ej3a1/ej3a1_main.py
@@ -1,27 +1,4 @@
 """
-Enunciado:
-Implementa un módulo llamado 'ej3a1_operations.py' que contenga funciones
-matemáticas básicas como 'add(num_1, num_2)', 'subtract(num_1, num_2)',
-'multiply(num_1, num_2)' y 'divide(num_1, num_2)'.
-El módulo 'ej3a1_operations.py' será llamado desde 'ej3a1_main.py'.
-
-Parámetros:
-    - num_1: valor númerico al cuál se realizará la operación.
-    - num_2: valor númerico al cuál se realizará la operación.
-
-Ejemplo:
-    Entrada:
-    add(100, 575)
-    subtract(15, 461)
-    multiply(13, 97)
-    divide(54, 12)
-
-    Salida:
-    675
-    -446
-    1261
-    4.5
-
 Enunciat:
 Implementa un mòdul anomenat 'ej3a1_operations.py' que contingui funcions
 matemàtiques bàsiques com 'add(num_1, num_2)', 'subtract(num_1, num_2)',
