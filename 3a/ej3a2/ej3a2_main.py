8702520123b61e37eb4f706fce5bb3fdecff32ed
ThisfrutaHD <268890942+ThisfrutaHD@users.noreply.github.com>
2026-04-21 15:45:43 +0200

    preparem enunciat

--- 3a/ej3a2/ej3a2_main.py
+++ 3a/ej3a2/ej3a2_main.py
@@ -1,38 +1,4 @@
 """
-Enunciado:
-Implementar un paquete llamado 'ej3a2_math_package' que tiene dos módulos:
-'arithmetic.py' y 'geometry.py', con la siguiente estructura:
-
-    ej3a2_math_package/
-        __init__.py
-        arithmetic.py
-        geometry.py
-
-El módulo 'arithmetic.py' contendrá dos funciones: 'power(base, exponent)',
-'square_root(number)'.
-El módulo 'geometry.py' contendrá funciones como: 'square_area(side_length)',
-'triangle_area(base_length, height)', 'circle_area(radius)' y
-'rectangle_area(base_length, height)'.
-
-El paquete 'ej3a2_math_package' se debe importar desde el script 'ej3a2_main.py'
-
-Ejemplo:
-    Entrada:
-    arithmetic.power(3, 2)
-    arithmetic.square_root(16)
-    geometry.square_area(5)
-    geometry.rectangle_area(3, 5)
-    geometry.triangle_area(3, 5)
-    geometry.circle_area(5)
-
-    Salida:
-    125
-    4.0
-    25
-    15
-    7.5
-    31.4159265359
-
 Enunciat:
 
 Implementar un paquet anomenat 'ej3a2_math_package' que té dos mòduls:
@@ -82,9 +48,8 @@ rectangle_area_result = geometry.rectangle_area(3, 5)
 triangle_area_result = geometry.triangle_area(3, 5)
 circle_area_result = geometry.circle_area(5)
 
-# Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
-#print(square_area_result)
-#print(rectangle_area_result)
-#print(triangle_area_result)
-#print(circle_area_result)
+print(square_area_result)
+print(rectangle_area_result)
+print(triangle_area_result)
+print(circle_area_result)
