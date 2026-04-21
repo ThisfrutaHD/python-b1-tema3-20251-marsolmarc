039f713f7b7f8b135644ca67d457e22a87c93578
ThisfrutaHD <268890942+ThisfrutaHD@users.noreply.github.com>
2026-04-21 17:16:48 +0200

    enunciat

--- 3a/ej3a3/ej3a3_main.py
+++ 3a/ej3a3/ej3a3_main.py
@@ -1,39 +1,4 @@
 """
-Enunciado:
-Se tiene el paquete 'ej3a3_time_package' el cual tiene dos módulos llamados:
-'convert_date_format' y 'date_operations', los módulos ya están hechos,
-por lo tanto, se requiere configurar el archivo '__init__.py' del paquete.
-Se tiene la siguiente estructura:
-
-    ej3a3_time_package/
-        __init__.py
-        convert_date_format.py
-        date_operations.py
-
-El módulo 'convert_date_format.py' contiene dos funciones:
-'string_to_datetime(date_string, date_format)',
-'datetime_to_string(date_time, date_format)'.
-
-El módulo 'date_operations.py' tiene funciones como: 'add_days(date, days)',
-'middle_day_between_two_dates(initial_date, final_date)'
-
-El paquete 'ej3a3_time_package' se debe importar desde el script 'ej3a3_main.py'
-por ello, se debe configurar el archivo '__init__.py'. Por tanto es necesario crear este archivo.
-
-Ejemplo:
-    Entrada:
-    convert_date_format.string_to_datetime("2023-12-25")
-    date_operations.add_days(datetime.datetime(2023, 12, 25, 0, 0), 10)
-    date_operations.middle_day_between_two_dates(
-        datetime.datetime(2023, 12, 25, 0, 0),
-        datetime.datetime(2024, 1, 4, 0, 0)
-    )
-
-    Salida:
-    2023-12-25 00:00:00
-    2024-01-04 00:00:00
-    2023-12-30 00:00:00
-
 Enunciat:
 
 Es té el paquet 'ej3a3_time_package' el qual té dos mòduls anomenats:
